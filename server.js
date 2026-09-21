cd522bb556d261912cabbfe70c6c3d31ba508628
Irene Asamany <YOUR-GITHUB-EMAIL>
2026-09-20 19:48:52 -0700

    Enable CORS for AMARA mobile app

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const OpenAI = require("openai");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 const client = new OpenAI({
